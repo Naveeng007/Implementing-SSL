bf51e74ae23ac0012e184609bc4be6e65a2df963
Rahul <rahulv0530@gmail.com>
2021-10-23 16:57:13 +0530

    Utils

---
+++ utils/sha1.py
@@ -0,0 +1,6 @@
+# SHA-1 or Secure Hash Algorithm 1 is a cryptographic hash function which takes an input and produces a 160-bit (20-byte) hash value.
+# This hash value is known as a message digest.
+# This message digest is usually then rendered as a hexadecimal number which is 40 digits long.
+
+
+# def sha1(text: str):
